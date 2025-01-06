b81022fd13228e6bbfba42430fc99af7da7a53ec
Kenlark <kenzokerachi@hotmail.fr>
2025-01-06 02:36:05 +0100

    fix sharedlayout

--- src/layout/SharedLayout.jsx
+++ src/layout/SharedLayout.jsx
@@ -1,19 +1,9 @@
-import { Outlet, useNavigation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import Navbar from "../components/Navbar.jsx";
 import Footer from "../components/Footer.jsx";
 
 function SharedLayout() {
-  const navigation = useNavigation;
-
-  if (navigation.state === "loading") {
-    return (
-      <main>
-        <div>Chargement...</div>
-      </main>
-    );
-  }
-
   return (
     <div className="layout">
       <Navbar />
